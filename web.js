5eac47750a08d8c88a9c013b546eff0c2e1bf412
Ubuntu <ubuntu@ip-172-31-24-205.us-west-2.compute.internal>
2013-07-10 17:08:33 +0000

    index html for web

--- web.js
+++ web.js
@@ -7,11 +7,16 @@ var content;
 fs.readFileSync('index.html', function(err, data){
 	if(err) return err;
 	content = data.toString('utf-8');
+	console.log(content);
+	app.get('/', function(request, response){
+		
+		response.send(content);
+	});
 });
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send(content);
-});
+});*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
